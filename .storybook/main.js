22bd5a23b6f32d77387d315656f0e9ad34fb7939
yudai64 <kemurin.yudai0723@gmail.com>
2023-01-07 15:30:27 +0900

    storybook 少しさわった

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,7 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+  stories: ['../src/components/**/*.stories.jsx'],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
